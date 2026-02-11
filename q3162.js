bd65c66da28659ec4a4585608e1879d918f21f2d
Vachhani-Tapan <tapan.vachhani.cg@gmail.com>
2026-02-11 10:19:17 +0530

    Problem Solved

---
+++ q3162.js
@@ -0,0 +1,12 @@
+function sol(nums1, nums2, k) {
+    let count = 0;
+    for (let i = 0; i < nums1.length; i++) {
+        for (let j = 0; j < nums2.length; j++) {
+            if ((nums1[i]) % (nums2[j] * k) == 0) {
+                count++
+            }
+        }
+    }
+    return count;
+}
+console.log(sol([1,3,4], [1,3,4], 1))
