b148c3fbba405364a87782635130c3532f048d49
Vachhani-Tapan <tapan.vachhani.cg@gmail.com>
2026-02-13 16:04:50 +0530

    Problem Solved

---
+++ q1822.js
@@ -0,0 +1,12 @@
+// function sol(nums) {
+//     let prod = 1
+//     for (const i of nums) {
+//         if(nums[i] == 0){
+//             return 0;
+//         }
+//         prod *= i
+//     }
+//     if(prod > 0) return 1;
+//     return -1;
+// }
+// console.log(sol([-1,1,-1,1,-1]))
