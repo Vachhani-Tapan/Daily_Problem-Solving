6ad0dd5b09deb91df62d9ec90c91dd47cc8cbd2c
Vachhani-Tapan <tapan.vachhani.cg@gmail.com>
2026-02-14 17:30:59 +0530

    Problem Solved

---
+++ q3110.js
@@ -0,0 +1,8 @@
+function sol(s) {
+    let sum = 0;
+    for (let i = 0; i < s.length-1; i++) {
+        sum += Math.abs(s.charCodeAt(i) - s.charCodeAt(i+1));
+    }
+    return sum;
+}
+console.log(sol("hello"));
