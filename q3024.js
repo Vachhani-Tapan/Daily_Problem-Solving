1f1fd2e69c53634ac3113ada5fe4857bdebca968
Vachhani-Tapan <tapan.vachhani.cg@gmail.com>
2026-02-14 09:12:23 +0530

    Problem Solved

---
+++ q3024.js
@@ -0,0 +1,15 @@
+function sol(nums) {
+    if(nums[0] + nums[1] > nums[2] && nums[0] + nums[2] > nums[1] && nums[2] + nums[1] > nums[0] ){
+        if(nums[0] == nums[1] && nums[1] == nums[2] ){
+            return "equilateral";
+        }
+        else if(nums[0] == nums[2] || nums[0] == nums[1] || nums[1] == nums[2]){
+            return "isosceles"
+        }
+        else{
+            return "scalene"
+        }
+    }
+    return "none"
+}
+console.log(sol([3,6,9]));
