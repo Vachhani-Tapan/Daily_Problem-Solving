8527e3d7591be7ddf28286a349b0797291c66be7
Vachhani-Tapan <tapan.vachhani.cg@gmail.com>
2026-02-17 21:18:23 +0530

    Problem Solved

---
+++ q507.js
@@ -0,0 +1,10 @@
+function sol(num) {
+    let sum = 0;
+    for (let i = 0; i < num; i++) {
+        if (num % i == 0) {
+            sum += i;
+        }
+    }
+    return sum == num;
+}
+console.log(sol(27));
