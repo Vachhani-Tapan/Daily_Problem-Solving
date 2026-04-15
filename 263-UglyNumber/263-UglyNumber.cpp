e58d33d4222f5008270442a3c96022f6e4f3139f
Vachhani Tapan <tapan.vachhani.cg@gmail.com>
2026-04-15 10:49:19 +0530

    Create 263-UglyNumber

---
+++ 263-UglyNumber/263-UglyNumber.cpp
@@ -0,0 +1,22 @@
+// Last updated: 4/15/2026, 10:49:19 AM
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n <= 0) return false;
+        while(n > 1){
+            if(n % 2 == 0){
+                n = n / 2;
+            }
+            else if(n % 3 == 0){
+                n = n / 3;
+            }
+            else if(n % 5 == 0){
+                n = n / 5;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
