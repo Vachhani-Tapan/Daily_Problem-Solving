cd6d5c05615cddae5f2f5548e8b61722a97b5044
Vachhani Tapan <tapan.vachhani.cg@gmail.com>
2026-04-15 10:49:05 +0530

    Create 378-KthSmallestElementInASortedMatrix

---
+++ 378-KthSmallestElementInASortedMatrix/378-KthSmallestElementInASortedMatrix.cpp
@@ -0,0 +1,14 @@
+// Last updated: 4/15/2026, 10:49:05 AM
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        vector<int> res;
+        for(int i = 0 ; i < matrix.size() ; i++){
+            for(int j = 0 ; j < matrix[i].size() ; j++){
+                res.push_back(matrix[i][j]);
+            }
+        }
+        sort(res.begin() , res.end());
+        return res[k-1];
+    }
+};
