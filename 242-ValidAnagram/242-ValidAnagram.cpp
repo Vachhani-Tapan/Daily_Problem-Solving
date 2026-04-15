27b1494616add697508d196078650beaec97af21
Vachhani Tapan <tapan.vachhani.cg@gmail.com>
2026-04-15 10:49:23 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.cpp
@@ -0,0 +1,18 @@
+// Last updated: 4/15/2026, 10:49:23 AM
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin() , s.end());
+        sort(t.begin() , t.end());
+        int i = 0;
+        int j = 0;
+        while(i < s.size()  || j < t.size()){
+            if(s[i] != t[j]){
+                return false;
+            }
+            i++;
+            j++;
+        }
+        return true;
+    }
+};
