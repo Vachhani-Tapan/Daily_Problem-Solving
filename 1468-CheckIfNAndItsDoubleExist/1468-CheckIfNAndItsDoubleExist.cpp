fcafab15e88feb0d77231ffe5443ccae920f1e65
Vachhani Tapan <tapan.vachhani.cg@gmail.com>
2026-04-15 10:48:13 +0530

    Create 1468-CheckIfNAndItsDoubleExist

---
+++ 1468-CheckIfNAndItsDoubleExist/1468-CheckIfNAndItsDoubleExist.cpp
@@ -0,0 +1,12 @@
+// Last updated: 4/15/2026, 10:48:13 AM
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        for(int i = 0 ; i < arr.size() ; i++){
+            for(int j = 0 ; j < arr.size() ; j++){
+                if(arr[j] == arr[i] * 2 && i != j) return true;
+            }
+        }
+        return false;
+    }
+};
