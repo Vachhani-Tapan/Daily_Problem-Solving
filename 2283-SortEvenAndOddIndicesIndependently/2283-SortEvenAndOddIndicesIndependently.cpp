b4a960d3380d78f4d62e2167667c768df5962b9d
Vachhani Tapan <tapan.vachhani.cg@gmail.com>
2026-04-15 10:46:46 +0530

    Create 2283-SortEvenAndOddIndicesIndependently

---
+++ 2283-SortEvenAndOddIndicesIndependently/2283-SortEvenAndOddIndicesIndependently.cpp
@@ -0,0 +1,25 @@
+// Last updated: 4/15/2026, 10:46:46 AM
+class Solution {
+public:
+    vector<int> sortEvenOdd(vector<int>& nums) {
+        vector <int> odd,even;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(i % 2 == 0){
+                even.push_back(nums[i]);
+            }
+            else{
+                odd.push_back(nums[i]);
+            }
+        }
+        sort(even.begin() , even.end());
+        sort(odd.begin() , odd.end());
+        reverse(odd.begin() , odd.end());
+        int e = 0;
+        int o = 0;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(i % 2 == 0) nums[i] = even[e++];
+            else nums[i] = odd[o++];
+        }
+        return nums;
+    }
+};
