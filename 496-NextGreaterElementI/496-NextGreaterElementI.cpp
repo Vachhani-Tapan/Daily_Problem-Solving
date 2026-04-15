d1fca11354dddb5fde5271d4b86a7820f896a6fa
Vachhani Tapan <tapan.vachhani.cg@gmail.com>
2026-04-15 10:48:53 +0530

    Create 496-NextGreaterElementI

---
+++ 496-NextGreaterElementI/496-NextGreaterElementI.cpp
@@ -0,0 +1,21 @@
+// Last updated: 4/15/2026, 10:48:53 AM
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        for (int i = 0; i < nums1.size(); i++) {
+            int ans = -1;
+            for (int j = 0; j < nums2.size(); j++) {
+                if (nums1[i] == nums2[j]) {
+                    for (int k = j + 1; k < nums2.size(); k++) {
+                        if (nums2[k] > nums1[i]) {
+                            ans = nums2[k];
+                            break;
+                        }
+                    }
+                }
+            }
+            nums1[i] = ans;
+        }
+        return nums1;
+    }
+};
