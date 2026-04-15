e027238e04837d896aeb4fd1987580f3ec8737e7
Vachhani Tapan <tapan.vachhani.cg@gmail.com>
2026-04-15 10:45:48 +0530

    Create 2727-NumberOfSeniorCitizens

---
+++ 2727-NumberOfSeniorCitizens/2727-NumberOfSeniorCitizens.cpp
@@ -0,0 +1,13 @@
+// Last updated: 4/15/2026, 10:45:47 AM
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count = 0;
+        for(int i = 0 ; i < details.size() ; i++){
+            if(details[i][11] >= '6' && details[i][12] > '0' || details[i][11] > '6'){
+                count++;
+            }
+        }
+        return count;
+    }
+};
