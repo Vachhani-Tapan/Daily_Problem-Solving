b90511a9aaa73676e3262a37923e216348a49eb7
Vachhani Tapan <tapan.vachhani.cg@gmail.com>
2026-04-15 10:45:59 +0530

    Create 2580-CircularSentence

---
+++ 2580-CircularSentence/2580-CircularSentence.cpp
@@ -0,0 +1,17 @@
+// Last updated: 4/15/2026, 10:45:59 AM
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+        int n = sentence.size();
+        if (sentence[0] != sentence[n - 1])
+            return false;
+        for (int i = 0; i < n - 1; i++) {
+            if (sentence[i] == ' ') {
+                if (sentence[i - 1] != sentence[i + 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
