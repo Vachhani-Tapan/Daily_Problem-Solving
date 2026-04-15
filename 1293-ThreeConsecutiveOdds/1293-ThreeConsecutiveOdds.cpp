bc4537813afa864bd1465940c0c87b017373f90d
Vachhani Tapan <tapan.vachhani.cg@gmail.com>
2026-04-15 10:48:24 +0530

    Create 1293-ThreeConsecutiveOdds

---
+++ 1293-ThreeConsecutiveOdds/1293-ThreeConsecutiveOdds.cpp
@@ -0,0 +1,12 @@
+// Last updated: 4/15/2026, 10:48:24 AM
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        for(int i = 2 ; i < arr.size() ; i++){
+            if((arr[i] % 2 != 0) && (arr[i-1] % 2 != 0) && (arr[i-2] % 2 != 0)){
+                return true;
+            }
+        }
+        return false;
+    }
+};
