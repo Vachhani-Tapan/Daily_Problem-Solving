526788638b7bbe8210890693bb7427b1091a57fc
Vachhani Tapan <tapan.vachhani.cg@gmail.com>
2026-04-15 10:47:18 +0530

    Create 2032-LargestOddNumberInString

---
+++ 2032-LargestOddNumberInString/2032-LargestOddNumberInString.cpp
@@ -0,0 +1,15 @@
+// Last updated: 4/15/2026, 10:47:18 AM
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        if(num.back() % 2 == 1) return num;
+        int i = num.size() - 1;
+        while(i >= 0){
+            if(num[i] % 2 == 1){
+                return num.substr(0 , i+1);
+            }
+            i--;
+        }
+        return "";
+    }
+};
