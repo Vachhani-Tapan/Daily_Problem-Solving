451329810da833616a24dfa0d423543c582d2a50
Vachhani Tapan <tapan.vachhani.cg@gmail.com>
2026-04-15 10:47:24 +0530

    Create 1987-SubstringsOfSizeThreeWithDistinctCharacters

---
+++ 1987-SubstringsOfSizeThreeWithDistinctCharacters/1987-SubstringsOfSizeThreeWithDistinctCharacters.cpp
@@ -0,0 +1,13 @@
+// Last updated: 4/15/2026, 10:47:24 AM
+class Solution {
+public:
+    int countGoodSubstrings(string s) {
+        int count = 0;
+        for(int i = 2 ; i < s.size() ; i++){
+            if((s[i] != s[i-1]) && (s[i-1] != s[i-2]) && (s[i] != s[i-2])){
+                count++;
+            }
+        }
+        return count;
+    }
+};
