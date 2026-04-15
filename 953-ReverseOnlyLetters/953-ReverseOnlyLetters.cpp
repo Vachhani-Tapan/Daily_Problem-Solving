cf8f70dc9a2f0e4e311b3e06620dff76084d4de1
Vachhani Tapan <tapan.vachhani.cg@gmail.com>
2026-04-15 10:48:36 +0530

    Create 953-ReverseOnlyLetters

---
+++ 953-ReverseOnlyLetters/953-ReverseOnlyLetters.cpp
@@ -0,0 +1,22 @@
+// Last updated: 4/15/2026, 10:48:35 AM
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int i = 0;
+        int j = s.size() - 1;
+        while(i <= j){
+            if(isalpha(s[i]) && isalpha(s[j])){
+                swap(s[i] , s[j]);
+                i++;
+                j--;
+            }
+            else if(!isalpha(s[i])){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        return s;
+    }
+};
