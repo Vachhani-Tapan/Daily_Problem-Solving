c3ecf3b9f04a2e7c1dce4581a774bffdb4d4a6e7
Vachhani Tapan <tapan.vachhani.cg@gmail.com>
2026-04-15 10:49:38 +0530

    Create 137-SingleNumberIi

---
+++ 137-SingleNumberIi/137-SingleNumberIi.cpp
@@ -0,0 +1,13 @@
+// Last updated: 4/15/2026, 10:49:38 AM
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin() , nums.end());
+        for(int i = 1 ; i < nums.size() ; i = i + 3){
+            if(nums[i] != nums[i-1]){
+                return nums[i-1];
+            }
+        }
+        return nums[nums.size() - 1];
+    }
+};
