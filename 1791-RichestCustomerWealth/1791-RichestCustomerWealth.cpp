7628f831be93933fbb04fef860f926f223feda18
Vachhani Tapan <tapan.vachhani.cg@gmail.com>
2026-04-15 10:47:47 +0530

    Create 1791-RichestCustomerWealth

---
+++ 1791-RichestCustomerWealth/1791-RichestCustomerWealth.cpp
@@ -0,0 +1,15 @@
+// Last updated: 4/15/2026, 10:47:47 AM
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int max1 = 0;
+        for(int i = 0 ; i < accounts.size() ; i++){
+            int sum = 0;
+            for(int j = 0 ; j < accounts[i].size() ; j++){
+                sum += accounts[i][j];
+            }
+            max1 = max(max1 , sum);
+        }
+        return max1;
+    }
+};
