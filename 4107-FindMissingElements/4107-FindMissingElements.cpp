016b213409372758fe417852cdb72214d5d9e297
Vachhani Tapan <tapan.vachhani.cg@gmail.com>
2026-04-15 10:44:08 +0530

    Create 4107-FindMissingElements

---
+++ 4107-FindMissingElements/4107-FindMissingElements.cpp
@@ -0,0 +1,20 @@
+// Last updated: 4/15/2026, 10:44:07 AM
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        vector<int> v1;
+        int j = 0; 
+        sort(nums.begin() , nums.end());
+        int min1 = *min_element(nums.begin() , nums.end());
+        int max1 = *max_element(nums.begin() , nums.end());
+        for(int i = min1 ; i <= max1 ; i++){
+            if(nums[j] == i){
+                j++;
+            }
+            else{
+                v1.push_back(i);
+            }
+        }
+        return v1;
+    }
+};
