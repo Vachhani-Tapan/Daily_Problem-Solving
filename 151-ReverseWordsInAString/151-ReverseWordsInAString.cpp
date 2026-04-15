9f49e6d0351ee950454ef4147fca6aab1c07f7b3
Vachhani Tapan <tapan.vachhani.cg@gmail.com>
2026-04-15 10:49:36 +0530

    Create 151-ReverseWordsInAString

---
+++ 151-ReverseWordsInAString/151-ReverseWordsInAString.cpp
@@ -0,0 +1,20 @@
+// Last updated: 4/15/2026, 10:49:36 AM
+class Solution {
+public:
+    string reverseWords(string s) {
+        reverse(s.begin(), s.end());
+        string ans = "";
+        for (int i = 0; i < s.size(); i++) {
+            string word = "";
+            while (i < s.size() && s[i] != ' ') {
+                word += s[i];
+                i++;
+            }
+            reverse(word.begin(), word.end());
+            if (word.size() > 0) {
+                ans += " " + word;
+            }
+        }
+        return ans.substr(1);
+    }
+};
