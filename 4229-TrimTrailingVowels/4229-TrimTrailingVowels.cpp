b9431859ed9abef5b2aada3731b9e1dd40d388e8
Vachhani Tapan <tapan.vachhani.cg@gmail.com>
2026-04-15 10:43:48 +0530

    Create 4229-TrimTrailingVowels

---
+++ 4229-TrimTrailingVowels/4229-TrimTrailingVowels.cpp
@@ -0,0 +1,15 @@
+// Last updated: 4/15/2026, 10:43:48 AM
+class Solution {
+public:
+    string trimTrailingVowels(string s) {
+        for(int i = s.size() - 1 ; i >= 0 ; i--){
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u'){
+                s.erase(i,1);
+            }
+            else{
+                break;
+            }
+        }
+        return s;
+    }
+};
