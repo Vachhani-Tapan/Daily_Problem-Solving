dc3fe47ebbceb68cedc07add8a9cf5b19c1ce593
Vachhani Tapan <tapan.vachhani.cg@gmail.com>
2026-05-04 07:55:10 +0530

    File updated at 5/4/2026, 7:55:10 AM

--- 48-RotateImage/48-RotateImage.cpp
+++ 48-RotateImage/48-RotateImage.cpp
@@ -1,17 +1,16 @@
-// Last updated: 4/15/2026, 10:49:51 AM
-class Solution {
-public:
-    void rotate(vector<vector<int>>& matrix) {
-        // Transpose the matrix row -> column && col -> row
-        for(int i = 0 ; i < matrix.size() ; i++){
-            for(int j = i ; j < matrix.size() ; j++){
-                swap(matrix[i][j] , matrix[j][i]);
-            }
-        }
-
-        // reverse the rows element
-        for(int i = 0 ; i < matrix.size() ; i++){
-            reverse(matrix[i].begin() , matrix[i].end());
-        }
-    }
-};
+// Last updated: 5/4/2026, 7:55:10 AM
+1class Solution {
+2public:
+3    void rotate(vector<vector<int>>& matrix) {
+4        // Transpose the matrix row -> column && col -> row
+5        for(int i = 0 ; i < matrix.size() ; i++){
+6            for(int j = i ; j < matrix.size() ; j++){
+7                swap(matrix[i][j] , matrix[j][i]);
+8            }
+9        }
+10        // reverse the rows element
+11        for(int i = 0 ; i < matrix.size() ; i++){
+12            reverse(matrix[i].begin() , matrix[i].end());
+13        }
+14    }
+15};
