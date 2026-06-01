fa4db96d614f0ae67a735ff7ad67af75aad7a879
Vachhani Tapan <tapan.vachhani.cg@gmail.com>
2026-06-01 18:01:35 +0530

    File updated at 6/1/2026, 6:01:39 PM

--- 3945-DigitFrequencyScore/3945-DigitFrequencyScore.cpp
+++ 3945-DigitFrequencyScore/3945-DigitFrequencyScore.cpp
@@ -1,17 +1,12 @@
-// Last updated: 6/1/2026, 5:59:06 PM
+// Last updated: 6/1/2026, 6:01:39 PM
 1class Solution {
 2public:
 3    int digitFrequencyScore(int n) {
-4        unordered_map<int,int> m1;
+4        int ans = 0;
 5        while(n){
-6            int dig = n % 10;
-7            m1[dig]++;
-8            n /= 10;
-9        }
-10        int ans = 0;
-11        for(auto k : m1){
-12            ans += k.first * k.second;
-13        }
-14        return ans;
-15    }
-16};
+6            ans += n % 10;
+7            n /= 10;
+8        }
+9        return ans;
+10    }
+11};
