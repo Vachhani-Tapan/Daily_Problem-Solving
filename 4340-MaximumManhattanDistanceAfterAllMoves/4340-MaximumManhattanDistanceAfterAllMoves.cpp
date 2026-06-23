8c790c98188d03783bc9edaa075dbd2c5838ff87
Vachhani Tapan <tapan.vachhani.cg@gmail.com>
2026-06-23 10:23:00 +0530

    Create 4340-MaximumManhattanDistanceAfterAllMoves

---
+++ 4340-MaximumManhattanDistanceAfterAllMoves/4340-MaximumManhattanDistanceAfterAllMoves.cpp
@@ -0,0 +1,17 @@
+// Last updated: 6/23/2026, 10:23:06 AM
+class Solution {
+public:
+    int maxDistance(string moves) {
+        int x = 0;
+        int y = 0;
+        int count = 0;
+        for(auto k : moves){
+            if(k == 'L') x--;
+            else if(k == 'R') x++;
+            else if(k == 'U') y++;
+            else if(k == 'D') y--;
+            else count++;
+        }
+        return abs(x) + abs(y) + count;
+    }
+};
