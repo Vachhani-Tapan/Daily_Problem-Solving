9c2e02df8029c47008cb515e87f5c9380f77b41e
Vachhani Tapan <tapan.vachhani.cg@gmail.com>
2026-06-23 10:27:34 +0530

    Create 1524-StringMatchingInAnArray

---
+++ 1524-StringMatchingInAnArray/1524-StringMatchingInAnArray.cpp
@@ -0,0 +1,16 @@
+// Last updated: 6/23/2026, 10:27:40 AM
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+        vector<string> res;
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = 0; j < words.size(); j++) {
+                if (i != j && words[j].find(words[i]) != string::npos) {
+                    res.push_back(words[i]);
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
