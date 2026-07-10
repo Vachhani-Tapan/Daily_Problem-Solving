af96f9e098e9084a0eccdc0b38fd3a89aadc38a3
Vachhani Tapan <tapan.vachhani.cg@gmail.com>
2026-07-10 22:04:28 +0530

    Time: 1 ms (2.92%), Space: 8.1 MB (7.93%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int findComplement(int n) {
+        if(n == 0) return 1;
+        // convert n to binary format
+        string bin = "";
+        while(n){
+            bin = char((n % 2) + '0') + bin;
+            n = n / 2;
+        }
+
+        // flip the bits of that binary number
+        for(int i = 0 ; i < bin.size() ; i++){
+            if(bin[i] == '0'){
+                bin[i] = '1';
+            }
+            else{
+                bin[i] = '0';
+            }
+        }
+        cout << bin << endl;
+
+        // convert the final complement binary to base 10
+        int ans = 0;
+        int prod = 1;
+        int i = bin.size() - 1;
+        while(i > 0){
+            if(bin[i] == '1'){
+                ans += prod ;
+            }
+            prod = prod * 2;
+            i--;
+        }
+        return ans;
+    }
+};
