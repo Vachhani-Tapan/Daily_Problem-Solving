92e9ecb1afbfac94ae7b943823ab0d27863680c6
Vachhani Tapan <tapan.vachhani.cg@gmail.com>
2026-09-17 11:32:06 +0530

    Time: 0 ms (100%), Space: 15 MB (59.4%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,26 +11,35 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+
+        // step - 01 count total node in list
         int count = 0;
         ListNode* temp = head;
         while(temp != NULL){
             count++;
             temp = temp->next;
         }
-        count -= n;
-        temp = head;
-        ListNode* prev = NULL;
 
+        // step - 02 remove the n from count to get the number of node whose next element we have to remove
+        count -= n;
+
+        // step - 03 check if the count is zero then we have to remove the 1st node for the edge case this if runs
         if(count == 0){
             head = head->next;
             delete temp;
             return head;
         }
 
+        ListNode* prev = NULL;
+        temp = head;
+
+        // step - 04 to count-- to get to the number prev to the node we have to delete and maintain prev and temp pointer 
         while(count--){
             prev = temp;
             temp = temp->next;
         }
+
+        // step - 05 break the node between the number before we have to remove and attach that with the next of the curr deleting node
         prev->next = temp->next;
         delete temp;
         return head;
